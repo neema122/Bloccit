f510f711cf51d9b27186f3a0fc3582391137fa29
Neema <neema122@gmail.com>
2015-02-16 06:07:40 +0000

    complete authorization assignment

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,4 +1,25 @@
 class PostPolicy < ApplicationPolicy
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.admin?
+        scope.all
+      else
+        scope.where(:published => true)
+      end
+    end
+  end
+
+  def update?
+    user.admin? or not post.published?
+  end
+end
 end
 
 def index?
