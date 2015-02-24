dd2860882f171c94b2381bdc8b9c5433ea49fb82
Neema <neema122@gmail.com>
2015-02-24 04:43:41 +0000

    Vote-checkpoint-assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,31 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
     mount_uploader :avatar, AvatarUploader
+  
+  def up_votes
+     votes.where(value: 1).count
+   end
+  
+  def down_votes
+    votes.where(value: -1).count
+  end
+  
+   def update_rank
+     age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
+     new_rank = points + age_in_days
+ 
+     update_attribute(:rank, new_rank)
+   end
+  has_many :votes, dependent: :destroy
   has_one :summary
   belongs_to :user
     belongs_to :topic
-  default_scope { order('created_at DESC') }
+  default_scope { order('rank DESC') }
   
    validates :title, length: { minimum: 5 }, presence: true
    validates :body, length: { minimum: 20 }, presence: true
-   validates :topic, presence: true
-   validates :user, presence: true
+   # validates :topic, presence: true
+   # validates :user, presence: true
   
   def markdown_title
     render_as_markdown(title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
   has_many :posts
     mount_uploader :avatar, AvatarUploader
   has_many :comments
+  has_many :votes
   
   def admin?
    role == 'admin'
