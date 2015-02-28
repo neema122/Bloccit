b6890d128b55e49c48fdeee7efe83881fe578e75
Neema <neema122@gmail.com>
2015-02-28 07:08:47 +0000

    complete creating with ajax assignment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,7 +4,7 @@ class Comment < ActiveRecord::Base
   
   after_create :send_favorite_emails
   
-  default_scope { order('updated_at DESC') }
+  default_scope { order('updated_at ASC') }
  
    private
  
